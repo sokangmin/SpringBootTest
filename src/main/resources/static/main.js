5abad133b7ca2a34bd1a867af0f6d45e3ef445ee
Administrator <Administrator@HIKKIN-PC.winitech.com>
2016-05-17 19:26:20 +0900

    main.js 수정

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,38 +1,96 @@
-React.render(
-		<div>
-			<nav className="navbar navbar-inverse navbar-fixed-top">
-		      <div className="container">
-		        <div className="navbar-header">
-		          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-		            <span className="sr-only">Toggle navigation</span>
-		            <span className="icon-bar"></span>
-		            <span className="icon-bar"></span>
-		            <span className="icon-bar"></span>
-		          </button>
-		          <a className="navbar-brand" href="/">winitech</a>
-		        </div>
-		        <div id="navbar" className="collapse navbar-collapse">
-		          <ul className="nav navbar-nav">
-		            <li className="active"><a href="#">Home</a></li>
-		            <li><a href="#about">About</a></li>
-		            <li><a href="#contact">Contact</a></li>
-		          </ul>
-		          <ul className="nav navbar-nav navbar-right">
-		            <li><a href="#">Sign up</a></li>
-		            <li><a href="#about">Sign in</a></li>
-		          </ul>
-		        </div>{/* .nav-collapse */}
-		      </div>
-		    </nav>
-		    
-		    <div className="container">
+var Nav = React.createClass({
+	render: function() {
+		return (
+				<nav className="navbar navbar-inverse navbar-fixed-top">
+			      <div className="container">
+			        <div className="navbar-header">
+			          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			            <span className="sr-only">Toggle navigation</span>
+			            <span className="icon-bar"></span>
+			            <span className="icon-bar"></span>
+			            <span className="icon-bar"></span>
+			          </button>
+			          <a className="navbar-brand" href="/">winitech</a>
+			        </div>
+			        <div id="navbar" className="collapse navbar-collapse">
+			          <ul className="nav navbar-nav">
+			            <li className="active"><a href="#">Home</a></li>
+			            <li><a href="#about">About</a></li>
+			            <li><a href="#contact">Contact</a></li>
+			          </ul>
+			          <ul className="nav navbar-nav navbar-right">
+			            <li><a href="#">Sign up</a></li>
+			            <li><a href="#about">Sign in</a></li>
+			          </ul>
+			        </div>{/* .nav-collapse */}
+			      </div>
+			    </nav>		
+		);
+	}
+});
+
+var Main =  React.createClass({
+	render: function() {
+		return (
+				<div>
+				<Nav/>
+			    
+			    <div className="container">
+		
+			      <div className="starter-template">
+			        <h1>Bootstrap starter template</h1>
+			        <p className="lead">Use this document as a way to quickly start any new project.<br/> 
+			        All you get is this text and a mostly barebones HTML document.
+			        </p>
+			      </div>
+			    </div>{/*.container*/}
+			 </div>	
+		);
+	}
+});
+
+var Sign =  React.createClass({
+	render: function() {
+		return (
+				<div>
+				<Nav/>
+			    
+			    <div className="container">
+		
+			      <div className="starter-template">
+			        <h1>Sign up</h1>
+			      </div>
+			    </div>{/*.container*/}
+			 </div>	
+		);
+	}
+});
+
+var App = React.createClass({
+	getInitialState: function() {
+		return {
+			route: window.location.hash.substr(1)
+		}
+	},
 	
-		      <div className="starter-template">
-		        <h1>Bootstrap starter template</h1>
-		        <p className="lead">Use this document as a way to quickly start any new project.<br/> 
-		        All you get is this text and a mostly barebones HTML document.
-		        </p>
-		      </div>
-		    </div>{/*.container*/}
-		 </div>
-		, document.body);
+	componentDidMount: function() {
+		window.addEventListener('hashchange', () => {
+			this.setState({
+				route: window.location.hash.substr(1)
+			});
+		})
+	},
+	
+	render: function() {
+		var Child;
+		switch (this.state.route) {
+		case 'home': Child = Main; break;
+		case 'signup' : Child = Sign; break;
+		default: Child = Main;
+		}
+		
+		return (<Child/>);
+	}
+});
+
+React.render(<App/>, document.body);
